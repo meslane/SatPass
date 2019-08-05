fcd24c02cb81a0c861b82c6d71a46e80e0820093
62fe50 <merrickslane@gmail.com>
2019-08-04 17:59:14 -0700

    added more error checking and optimizations

--- SatPass.py
+++ SatPass.py
@@ -17,15 +17,20 @@ with open('tracked_satellites.txt') as s:
     
 with open('location.txt') as l:
     locdata = l.read().splitlines()
-    lat = str(locdata[0])
-    long = str(locdata[1])
-    alt = str(locdata[2])
+    lat     = str(locdata[0])
+    long    = str(locdata[1])
+    alt     = str(locdata[2])
     mindegs = str(locdata[3])
 
 for id in satellites:
-    query = url + "/radiopasses/{}/{}/{}/{}/1/{}".format(id,lat,long,lat,mindegs) + key
-    page = urlopen(query).read()
-    data = json.loads(page)
+    try:
+        query = url + "/radiopasses/{}/{}/{}/{}/1/{}".format(id,lat,long,lat,mindegs) + key
+        page = urlopen(query).read()
+        data = json.loads(page)
+    except urllib.error.URLError:
+        print("Unable to connect to server\n")
+        input("Press ENTER to continue")
+        exit(1)
  
     name = str(data["info"]["satname"])
     passes = str(data["info"]["passescount"])
@@ -37,8 +42,9 @@ for id in satellites:
         try:
             startutc = datetime.utcfromtimestamp(int(data["passes"][i]["startUTC"]))
             start = startutc.replace(tzinfo=pytz.utc).astimezone(local_tz).strftime('%Y-%m-%d %I:%M %p')
-            elev = str(data["passes"][i]["maxEl"])
-            print("{}, {} DEG".format(start, elev))
+            elev = float(data["passes"][i]["maxEl"])
+            dir = str(data["passes"][i]["startAzCompass"])
+            print("{0}, {1:.2f} DEG {2}".format(start, elev, dir))
             i+=1
         except (IndexError, KeyError) as e:
             break
